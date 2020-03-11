c027cf4e549d0dfdce0ce4e4f6dd28108f7d9eac
Valtteri Karesto <valtteri.karesto@gmail.com>
2020-03-11 14:42:54 +0200

    Added network graphs

---
+++ components/NetworkGraph.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import Graph from "react-graph-vis";
+
+const NetworkGraph = ({ data }) => {
+  const [isComponentMounted, setIsComponentMounted] = useState(false)
+
+  useEffect(() => setIsComponentMounted(true), [])
+
+  if(!isComponentMounted) {
+    return null
+  }
+  const options = {
+    layout: {
+      hierarchical: false
+    },
+    edges: {
+      color: "#000000",
+      width: 2,
+      smooth: true,
+    },
+    height: "600px",
+    nodes: {
+      // shape: "dot",
+      size: 30,
+      font: {
+        size: 22,
+        face: 'Space Mono',
+        color: "#fff"
+      },
+      borderWidth: 2
+    },
+    autoResize: true, 
+    layout: {
+      improvedLayout: true,
+      clusterThreshold: 150,
+    }
+  };
+
+  const events = {
+    select: function(event) {
+      var { nodes, edges } = event;
+    }
+  };
+  return (
+    <Graph
+      graph={data}
+      options={options}
+      events={events}
+    />
+  );
+}
+
+export default NetworkGraph;
--- utils/chartDataHelper.ts
+++ utils/chartDataHelper.ts
@@ -2,9 +2,20 @@ import { format, sub, eachDayOfInterval, isSameDay } from 'date-fns';
 import groupBy from 'lodash.groupby'
 import sortBy from 'lodash.sortby'
 import { Confirmed } from '../pages';
+import { InfectionSourceEnum } from '../pages/index';
 
 // Map data to show development of infections
-
+export const colors = [
+  '#003f5c',
+  '#2fab8e',
+  '#665191',
+  '#a05195',
+  '#d45087',
+  '#f95d6a',
+  '#ff7c43',
+  '#ffa600',
+  '#ee2320',
+];
 
 interface InfectionDevelopmentDataItem {
   date: number;
@@ -70,5 +81,49 @@ export const getInfectionsBySourceCountry = (confirmed:Confirmed[]) => {
   return {
     infectionsBySourceCountry,
     areas
+  };
+}
+
+const getGroup = (infection: Confirmed, confirmed: Confirmed[]): string | null => {
+  if (typeof infection.infectionSource === 'number') {
+    const item = confirmed.find(i => Number(i.id) === infection.infectionSource);
+    if (item) {
+      if (typeof item.infectionSource === 'number') {
+        return getGroup(item, confirmed);
+      }
+      if (item.infectionSourceCountry) {
+        return item.infectionSourceCountry;
+      }
+      return null;
+    }
+  }
+  return infection.infectionSourceCountry;
+}
+
+export const getNetworkGraphData = (confirmed:Confirmed[]) => {
+
+  const infectionSources = Array.from(new Set(confirmed.filter(i => typeof i.infectionSource === 'number').map(inf => inf.infectionSource)));
+
+  const uniqueCountries = Array.from(new Set(confirmed.filter(i => !!i.infectionSourceCountry).map(inff => inff.infectionSourceCountry)));
+
+  const allNodes = confirmed.map((infection, index) => ({
+    ...infection,
+    id: Number(infection.id),
+    label: `#${infection.id}`,
+    // group: getGroup(infection, confirmed),
+    color: `${colors[uniqueCountries.indexOf(infection.infectionSourceCountry)]}`,
+  }));
+  const nodes = allNodes.filter(i => infectionSources.includes(i.id) || typeof i.infectionSource === 'number');
+  // @ts-ignore
+  const edges = allNodes.map(i => ({
+    from: typeof i.infectionSource === 'number' ? i.infectionSource : i.infectionSourceCountry,
+    to: i.id,
+  }));
+  const filteredNodes = allNodes.filter(i => !!i.infectionSourceCountry || typeof i.infectionSource === 'number');
+  // @ts-ignore
+  uniqueCountries.map((country, index) => filteredNodes.push({ id:country, label: country, color: `${colors[uniqueCountries.indexOf(country)]}` }))
+  return {
+    nodes: filteredNodes,
+    edges
   };
 }
