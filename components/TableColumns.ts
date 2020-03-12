a095e5f46cc23186b0dda4c66cf7f6e6b863d826
Valtteri Karesto <valtteri.karesto@gmail.com>
2020-03-12 11:55:55 +0200

    Remove id since it is not accurate atm

--- components/TableColumns.ts
+++ components/TableColumns.ts
@@ -1,10 +1,10 @@
 import { format } from 'date-fns';
 
 export const infectionColumns = [
-  {
-    Header: 'ID',
-    accessor: 'id',
-  },
+  // {
+  //   Header: 'ID',
+  //   accessor: 'id',
+  // },
   {
     Header: 'Päiväys',
     accessor: 'date',
